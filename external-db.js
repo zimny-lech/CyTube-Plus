a912b64c0d796fa23e8bcd3aa1df303f4bf634dc
Zimny Lech <zimnyleszek1@trashbox.pl>
2013-12-29 23:19:55 +0100

    Version 2.5

--- external-db.js
+++ external-db.js
@@ -1,6 +1,6 @@
 /* ----- VIDEO DATABASE CONFIGURATION ----- */
 
-videos=[
+ChannelDatabase_Array=[
 ['', 'Japanese'],
 ['http://www.youtube.com/watch?v=Q3y-80HBM6Q', 'An Cafe - Smile Ichiban Ii Onna'],
 ['http://www.youtube.com/watch?v=nDqaTXqCN-Q', 'Babymetal - Ijime, Dame, Zettai'],
@@ -33,115 +33,52 @@ videos=[
 ['http://www.youtube.com/watch?v=jehMXrY1q5I', 'Yui Makino - Yume No Tsubasa'],
 ];
 
-// DESCRIPTION: videos database array (additional set of categorized videos/songs)
+// DESCRIPTION: videos database array (additional set of categorized media)
 // ARRAY SYNTAX: ['video_URL', 'title'],
-// REQUIRE: channelDatabase enabled
-// NOTE: leave empty '' video_URL field to create section button
+// REQUIRE: UI_ChannelDatabase enabled
+// NOTE: leave empty '' in the "video_URL" field to create section button
 
 /* ----- CODE BELOW MUST BE INCLUDED IN EXTERNAL FILE ----- */
 
-$("#userpltogglewrap").after('<div id="dbtoggle-outer" class="well well-small span12 row-fluid"/>');
-$("#dbtoggle-outer").append('<div id="dbtoggle" class="span12 pointer"/>');
-$("#dbtoggle").html('<i class="icon-plus pull-left"></i><p>Show Channel Database</p>');
+// adding Channel Database box
 
-$("#dbtoggle").after('<div id="dbwrap" class="span12"/>');
-$("#dbwrap").css('display', 'none').css('margin-left', '0').css('margin-bottom', '5px');
-generateToggle("#dbtoggle", "#dbwrap");
+if (UI_ChannelGalleries=="1") {
+	$_dbtoggleouter=$('<div id="dbtoggle-outer" class="well well-small span12 row-fluid" />')
+ 	 .insertBefore($_gallerytoggleouter);
+} else {
+	$_dbtoggleouter=$('<div id="dbtoggle-outer" class="well well-small span12 row-fluid" />')
+  	.appendTo("#leftpane-inner");
+}
+$_dbtoggle=$('<div id="dbtoggle" class="span12 pointer" />')
+  .html('<i class="icon-plus pull-left"></i><p>Show Channel Database</p>')
+  .appendTo($_dbtoggleouter);
+$_dbwrap=$('<div id="dbwrap" class="span12" />')
+  .css('display', 'none').css('margin-left', '0')
+  .insertAfter($_dbtoggle);
 
+var isDB=false;
 var item_nr=0;
-var layer_nr=0;
+var layer_nr=1;
 var opening=new Array();
-for(i=0; i<videos.length; i++) {
-	if(videos[i][0]=="") {
-		if(layer_nr>0) {
-			$("#dbwrap").append(list);
-			$("#la"+layer_nr).append(' <span class="songs-count">['+count_nr+']</span>');
-			opening[layer_nr-1]=0;
-		}
-		layer_nr++;
-		count_nr=0;
-		btn=$('<button id="la'+layer_nr+'" style="width:100%" onClick="toggleCat('+layer_nr+')"/>');
-		btn.addClass('btn btn-small db-break');
-		btn.text(videos[i][1]);
-		$("#dbwrap").append(btn);
-		list=$('<ul id="l'+layer_nr+'" style="max-height:410px; overflow:auto; margin-bottom:0px"/>');
-		list.addClass('videolist db-cat');
+var item_count=new Array(0);
+var count_nr=0;
+
+if (ChannelDatabase_Array.length<1 || ChannelDatabase_Array[0][0]!="") {
+	ChannelDatabase_Array.unshift(['', '<i>non-classified</i>']);
+}
+UI_ChannelCache=="1" ? createDatabase() : '';
+
+// click: toggle channel database
+
+$("#dbtoggle").on("click", function() {
+	if ($("#dbwrap").css("display")=="none") {
+		$("#dbwrap").show();
+		!isDB ? createDatabase() : '';
+		$("#dbtoggle").html($("#dbtoggle").html().replace(/Show/, "Hide"));
+		$("#dbtoggle i").removeClass("icon-plus").addClass("icon-minus");
 	} else {
-		item_nr++;
-		count_nr++;
-		item=$('<li class="queue_entry"/>');
-		btn=$('<button onClick="addVideo(\''+videos[i][0]+'\')"/>');
-		btn.addClass('btn btn-mini qe_btn pull-right');
-		btn.text('End');
-		item.append(btn);
-		parsed=parseMediaLink(videos[i][0]);
-		if(parsed["type"]=="yt") {
-			a=parsed["id"];
-			btn2=$('<button onClick="prevVideo(\''+a+'\')"/>');
-			btn2.addClass('btn btn-mini qe_btn pull-right');
-			btn2.append('<i class="icon-film"></i>');
-			item.append(btn2);
-		}
-		item.append('<span class="badge db-badge">'+item_nr+'</span>');
-		item.append('<span class="db-title" style="margin-left:4px">'+videos[i][1]+'</span><br/>');
-		item.append('<span class="db-link">'+videos[i][0]+'</span>');
-		list.append(item);
+		$("#dbwrap").hide();
+		$("#dbtoggle").html($("#dbtoggle").html().replace(/Hide/, "Show"));
+		$("#dbtoggle i").removeClass("icon-minus").addClass("icon-plus");
 	}
-}
-$("#dbwrap").append(list);
-$("#la"+layer_nr).append(' <span class="songs-count">['+count_nr+']</span>');
-opening[layer_nr-1]=0;
-
-// function: toggle database sections
-
-function toggleCat(a) {
-	b=a-1;
-	if(opening[b]==0) {
-		closeAll();
-		$("#l"+a).show();
-		opening[b]=1;
-	} else {
-		$("#l"+a).hide();
-		opening[b]=0;
-	}
-}
-
-// function: close all database sections
-
-function closeAll() {
-	$(".db-cat").hide();
-	for(i=0; i<opening.length; i++) {
-		opening[i]=0;
-	}
-}
-closeAll();
-
-// function: add database link to playlist
-
-var addedLinks=new Array();
-function addVideo(link) {
-	parsed=parseMediaLink(link);
-	idx=parsed["id"];
-	if(idx!=null) {
-		a=(new Date()).getTime();
-		if(!hasPermission("playlistadd")) {
-			alert('You have no permission to add a link.');
-		} else if(addedLinks[idx]!=undefined && a-addedLinks[idx]<120000) {
-			alert('You have just added this link.');
-		} else {
-			socket.emit("queue", {id: idx, pos: "end", type: parsed["type"]});
-			addedLinks[idx]=a;
-		}
-	}
-}
-
-// function: preview video
-
-function prevVideo(a) {
-	createModal('Preview Video', 'Close');
-
-	player=$('<iframe/>');
-	player.attr('src', 'http://www.youtube.com/embed/'+a+'?enablejsapi=1')
-	  .attr('width', '530').attr('height', '323').attr('frameborder', '0');
-	player.appendTo(body);
-}
+});
