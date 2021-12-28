4c6f3e2081b3aaf508ba3a9144003277a4d30d31
papertek <littlerock894@gmail.com>
2021-12-28 16:36:57 -0600

    Added a little note to see if i know what im doing lol

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 The MIT License (MIT)
 //
 Copyright (c) 2013-2014 Zimny Lech
-//
+
+// this is a test to see if im not that pepega -john
+
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
 the Software without restriction, including without limitation the rights to
