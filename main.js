215a3dbc6f12bf12bddbeb1174fa868e7304cc77
airforce270 <96109779+airforce270@users.noreply.github.com>
2021-12-30 20:36:30 -0600

    Bump version

--- main.js
+++ main.js
@@ -182,7 +182,7 @@ const MiniLogo_URL = 'https://cdn.7tv.app/emote/614e8c0b20eaf897465a4c9d/1x';
 
 const ChannelName_Caption = 'CyDJ';
 
-const Version_Now = 'CyDJBeta12.30.21.0';
+const Version_Now = 'CyDJBeta12.30.21.1';
 
 const HeaderDropMenu_Title = 'Information';
 
