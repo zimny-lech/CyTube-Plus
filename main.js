476fd8100ab0aa60eeb08ef2ed6cfe8b90a9ae98
airforce270 <96109779+airforce270@users.noreply.github.com>
2022-01-15 14:17:19 -0800

    Fix attribute retrieval

--- main.js
+++ main.js
@@ -449,190 +449,190 @@ class Badge {
 }
 
 const USER_BADGES = {
-    'ZimnLech': [
-        'https://dl.dropboxusercontent.com/s/89gv8fvyr49vnev/kobato-icon-small.jpg',
-    ],
-    'JohnRG123': [
-        Badge.NINETEEN_EIGHTY_FOUR,
-        'https://cdn.discordapp.com/attachments/899888900611375184/901313787951480862/small.png',
-    ],
-    'mrfart2323': [
-        Badge.SUB_9_MONTHS_T1,
-        'https://cdn.discordapp.com/attachments/899888900611375184/917638191723450418/Gigachad_1.jpg',
-    ],
-    'IP0G': [
-        Badge.BITS_100000,
-        'https://cdn.discordapp.com/attachments/679270173567614987/907384062807789598/1_25x25.gif',
-    ],
-    'ACCOUNT2': [
-        Badge.SUB_12_MONTHS_T1,
-        'https://media.discordapp.net/attachments/899888900611375184/901380813243944970/EZ_1.png',
-    ],
-    'Zombsiee': [
-        'https://media.discordapp.net/attachments/899888900611375184/901570550034477076/st_small_507x507-pad_600x600_f8f8f8-removebg-preview.png',
-    ],
-    'GiveMeAnOnion': [
-        Badge.SUB_18_MONTHS_T1,
-        'https://images-ext-1.discordapp.net/external/V-pZ97hRKTpvONRbEeYiG2tdmVBH11KU0u2KooeIU1g/https/i.imgur.com/CTJoS3c.png',
-    ],
-    'jensai1': [
-        'https://cdn.discordapp.com/attachments/915656975696687124/916100466344099890/cheer_1.png',
-        'https://cdn.discordapp.com/attachments/904501042148958219/916844473051389972/ezgif-3-79f6e07d0543.gif',
-    ],
-    'ContraVz': [
-        'https://media.discordapp.net/attachments/899888900611375184/903429579274846248/VIPBadge.png',
-        'https://cdn.discordapp.com/attachments/915656975696687124/916100799233425548/VIP.png',
-    ],
-    'seagrade': [
-        Badge.SUB_30_MONTHS_T1,
-        'https://cdn.discordapp.com/attachments/907369587979460720/917657328013176833/dankspin3.gif',
-    ],
-    'Xsogon': [
-        Badge.SUB_18_MONTHS_T1,
-        'https://cdn.discordapp.com/attachments/905159869626187817/920394576999637042/Porcellio_Expansus_PFP.png',
-    ],
-    'spintto': [
-        'https://cdn.discordapp.com/attachments/679270173567614987/908197729249919006/ezgif-4-78928f8a284c.gif',
-    ],
-    'HentaiQueen': [
-        'https://cdn.discordapp.com/attachments/679270173567614987/908795988544729198/ezgif-6-268375e001ab.gif',
-    ],
-    'gustme': [
-        Badge.SUB_9_MONTHS_T1,
-        'https://cdn.discordapp.com/attachments/915650094697693184/921144650088194098/ezgif.com-gif-maker_27.gif',
-    ],
-    'BlooDerg': [
-        'https://media.discordapp.net/attachments/880765465318817852/914342333414244372/ok.gif',
-    ],
-    'jesus_knuckles': [
-        'https://media.discordapp.net/attachments/914591356003946507/914592520305012796/stupidskullfart.png',
-    ],
-    'airforce2700': [
-        Badge.SUB_9_MONTHS_T1,
-        'https://cdn.betterttv.net/emote/6091a20a39b5010444d0b980/1x.gif',
-    ],
-    'Skully_OW': [
-        Badge.SUB_30_MONTHS_T1,
-        'https://cdn.discordapp.com/attachments/915656975696687124/916100799233425548/VIP.png',
-    ],
-    'n3r1': [
-        Badge.SUB_18_MONTHS_T1,
-    ],
-    'notjuham': [
-        Badge.SUB_30_MONTHS_T1,
-        'https://cdn.discordapp.com/attachments/891695154333884447/922151274303070229/notjuham1818.png',
-    ],
-    'Pugnator': [
-        Badge.SUB_3_MONTHS_T1,
-    ],
-    'IIPsilocybin': [
-        Badge.GIFT_SUBS_500,
-    ],
-    'Uns1asher': [
-        Badge.SUB_9_MONTHS_T1,
-    ],
-    'jeffboys123': [
-        Badge.SUB_24_MONTHS_T1,
-    ],
-    'alyjiah17': [
-        Badge.OWL,
-    ],
-    'brcfrmn': [
-        Badge.SUB_12_MONTHS_T1,
-    ],
-    'Frostee115': [
-        Badge.SUB_0_MONTHS_T1,
-    ],
-    'moldnugget': [
-        Badge.SUB_12_MONTHS_T1,
-    ],
-    'eljulidi1337': [
-        Badge.SUB_6_MONTHS_T3,
-        'https://cdn.discordapp.com/attachments/891695154333884447/922146791862988810/ezgif.com-gif-maker_28.gif',
-    ],
-    'pepegaOverheat': [
-        Badge.SUB_6_MONTHS_T1,
-    ],
-    'Captkayy': [
-        Badge.SUB_6_MONTHS_T3,
-    ],
-    'PeepoGlad': [
-        Badge.SUB_0_MONTHS_T1,
-    ],
-    'MrNiTe': [
-        Badge.SUB_6_MONTHS_T1,
-    ],
-    'l_charles_l': [
-        Badge.SUB_3_MONTHS_T1,
-    ],
-    'Enzo_SuperCraftZ': [
-        Badge.SUB_12_MONTHS_T1,
-    ],
-    'Physikz': [
-        Badge.SUB_36_MONTHS_T1,
-        'https://cdn.discordapp.com/attachments/915656975696687124/916100799233425548/VIP.png',
-    ],
-    'LEBB': [
-        Badge.SUB_12_MONTHS_T1,
-    ],
-    'feelssunnyman': [
-        Badge.SUB_9_MONTHS_T3,
-        'https://cdn.discordapp.com/attachments/915656975696687124/916100799233425548/VIP.png',
-    ],
-    'WWWilt': [
-        Badge.SUB_3_MONTHS_T1,
-    ],
-    'T_Nizzy': [
-        Badge.SUB_6_MONTHS_T1,
-    ],
-    'Getronix': [
-        Badge.SUB_12_MONTHS_T1,
-    ],
-    'Jamoki_3090': [
-        Badge.SUB_3_MONTHS_T1,
-    ],
-    'BryG05': [
-        Badge.SUB_3_MONTHS_T1,
-    ],
-    'SenileCookie': [
-        Badge.SUB_18_MONTHS_T1,
-    ],
-    'BryG05': [
-        Badge.SUB_3_MONTHS_T1,
-    ],
-    'SenileCookie': [
-        Badge.SUB_18_MONTHS_T1,
-    ],
-    'isiete': [
-        Badge.SUB_6_MONTHS_T1,
-    ],
-    'xQt0001': [
-        Badge.SUB_12_MONTHS_T3,
-    ],
-    'ayyvollet': [
-        Badge.SUB_3_MONTHS_T1,
-    ],
-    'Tzuqi': [
-        Badge.SUB_9_MONTHS_T1,
-    ],
-    'MrDestructoidCyDJ': [
-        'https://cdn.discordapp.com/attachments/679270173567614987/922352061356572682/1x_4.gif',
-    ],
-    'CaribbeanCam': [
-        Badge.SUB_12_MONTHS_T1,
-    ],
-    'Bananaman3100': [
-        Badge.SUB_6_MONTHS_T1,
-    ],
-    'Betelgeun': [
-        Badge.SUB_12_MONTHS_T1,
-    ],
-    'HikkenNoAce': [
-        Badge.SUB_6_MONTHS_T3,
-    ],
-    'CannabisMuffin': [
-        Badge.SUB_18_MONTHS_T1,
-    ],
+  'ZimnLech': [
+    'https://dl.dropboxusercontent.com/s/89gv8fvyr49vnev/kobato-icon-small.jpg',
+  ],
+  'JohnRG123': [
+    Badge.NINETEEN_EIGHTY_FOUR,
+    'https://cdn.discordapp.com/attachments/899888900611375184/901313787951480862/small.png',
+  ],
+  'mrfart2323': [
+    Badge.SUB_9_MONTHS_T1,
+    'https://cdn.discordapp.com/attachments/899888900611375184/917638191723450418/Gigachad_1.jpg',
+  ],
+  'IP0G': [
+    Badge.BITS_100000,
+    'https://cdn.discordapp.com/attachments/679270173567614987/907384062807789598/1_25x25.gif',
+  ],
+  'ACCOUNT2': [
+    Badge.SUB_12_MONTHS_T1,
+    'https://media.discordapp.net/attachments/899888900611375184/901380813243944970/EZ_1.png',
+  ],
+  'Zombsiee': [
+    'https://media.discordapp.net/attachments/899888900611375184/901570550034477076/st_small_507x507-pad_600x600_f8f8f8-removebg-preview.png',
+  ],
+  'GiveMeAnOnion': [
+    Badge.SUB_18_MONTHS_T1,
+    'https://images-ext-1.discordapp.net/external/V-pZ97hRKTpvONRbEeYiG2tdmVBH11KU0u2KooeIU1g/https/i.imgur.com/CTJoS3c.png',
+  ],
+  'jensai1': [
+    'https://cdn.discordapp.com/attachments/915656975696687124/916100466344099890/cheer_1.png',
+    'https://cdn.discordapp.com/attachments/904501042148958219/916844473051389972/ezgif-3-79f6e07d0543.gif',
+  ],
+  'ContraVz': [
+    'https://media.discordapp.net/attachments/899888900611375184/903429579274846248/VIPBadge.png',
+    'https://cdn.discordapp.com/attachments/915656975696687124/916100799233425548/VIP.png',
+  ],
+  'seagrade': [
+    Badge.SUB_30_MONTHS_T1,
+    'https://cdn.discordapp.com/attachments/907369587979460720/917657328013176833/dankspin3.gif',
+  ],
+  'Xsogon': [
+    Badge.SUB_18_MONTHS_T1,
+    'https://cdn.discordapp.com/attachments/905159869626187817/920394576999637042/Porcellio_Expansus_PFP.png',
+  ],
+  'spintto': [
+    'https://cdn.discordapp.com/attachments/679270173567614987/908197729249919006/ezgif-4-78928f8a284c.gif',
+  ],
+  'HentaiQueen': [
+    'https://cdn.discordapp.com/attachments/679270173567614987/908795988544729198/ezgif-6-268375e001ab.gif',
+  ],
+  'gustme': [
+    Badge.SUB_9_MONTHS_T1,
+    'https://cdn.discordapp.com/attachments/915650094697693184/921144650088194098/ezgif.com-gif-maker_27.gif',
+  ],
+  'BlooDerg': [
+    'https://media.discordapp.net/attachments/880765465318817852/914342333414244372/ok.gif',
+  ],
+  'jesus_knuckles': [
+    'https://media.discordapp.net/attachments/914591356003946507/914592520305012796/stupidskullfart.png',
+  ],
+  'airforce2700': [
+    Badge.SUB_9_MONTHS_T1,
+    'https://cdn.betterttv.net/emote/6091a20a39b5010444d0b980/1x.gif',
+  ],
+  'Skully_OW': [
+    Badge.SUB_30_MONTHS_T1,
+    'https://cdn.discordapp.com/attachments/915656975696687124/916100799233425548/VIP.png',
+  ],
+  'n3r1': [
+    Badge.SUB_18_MONTHS_T1,
+  ],
+  'notjuham': [
+    Badge.SUB_30_MONTHS_T1,
+    'https://cdn.discordapp.com/attachments/891695154333884447/922151274303070229/notjuham1818.png',
+  ],
+  'Pugnator': [
+    Badge.SUB_3_MONTHS_T1,
+  ],
+  'IIPsilocybin': [
+    Badge.GIFT_SUBS_500,
+  ],
+  'Uns1asher': [
+    Badge.SUB_9_MONTHS_T1,
+  ],
+  'jeffboys123': [
+    Badge.SUB_24_MONTHS_T1,
+  ],
+  'alyjiah17': [
+    Badge.OWL,
+  ],
+  'brcfrmn': [
+    Badge.SUB_12_MONTHS_T1,
+  ],
+  'Frostee115': [
+    Badge.SUB_0_MONTHS_T1,
+  ],
+  'moldnugget': [
+    Badge.SUB_12_MONTHS_T1,
+  ],
+  'eljulidi1337': [
+    Badge.SUB_6_MONTHS_T3,
+    'https://cdn.discordapp.com/attachments/891695154333884447/922146791862988810/ezgif.com-gif-maker_28.gif',
+  ],
+  'pepegaOverheat': [
+    Badge.SUB_6_MONTHS_T1,
+  ],
+  'Captkayy': [
+    Badge.SUB_6_MONTHS_T3,
+  ],
+  'PeepoGlad': [
+    Badge.SUB_0_MONTHS_T1,
+  ],
+  'MrNiTe': [
+    Badge.SUB_6_MONTHS_T1,
+  ],
+  'l_charles_l': [
+    Badge.SUB_3_MONTHS_T1,
+  ],
+  'Enzo_SuperCraftZ': [
+    Badge.SUB_12_MONTHS_T1,
+  ],
+  'Physikz': [
+    Badge.SUB_36_MONTHS_T1,
+    'https://cdn.discordapp.com/attachments/915656975696687124/916100799233425548/VIP.png',
+  ],
+  'LEBB': [
+    Badge.SUB_12_MONTHS_T1,
+  ],
+  'feelssunnyman': [
+    Badge.SUB_9_MONTHS_T3,
+    'https://cdn.discordapp.com/attachments/915656975696687124/916100799233425548/VIP.png',
+  ],
+  'WWWilt': [
+    Badge.SUB_3_MONTHS_T1,
+  ],
+  'T_Nizzy': [
+    Badge.SUB_6_MONTHS_T1,
+  ],
+  'Getronix': [
+    Badge.SUB_12_MONTHS_T1,
+  ],
+  'Jamoki_3090': [
+    Badge.SUB_3_MONTHS_T1,
+  ],
+  'BryG05': [
+    Badge.SUB_3_MONTHS_T1,
+  ],
+  'SenileCookie': [
+    Badge.SUB_18_MONTHS_T1,
+  ],
+  'BryG05': [
+    Badge.SUB_3_MONTHS_T1,
+  ],
+  'SenileCookie': [
+    Badge.SUB_18_MONTHS_T1,
+  ],
+  'isiete': [
+    Badge.SUB_6_MONTHS_T1,
+  ],
+  'xQt0001': [
+    Badge.SUB_12_MONTHS_T3,
+  ],
+  'ayyvollet': [
+    Badge.SUB_3_MONTHS_T1,
+  ],
+  'Tzuqi': [
+    Badge.SUB_9_MONTHS_T1,
+  ],
+  'MrDestructoidCyDJ': [
+    'https://cdn.discordapp.com/attachments/679270173567614987/922352061356572682/1x_4.gif',
+  ],
+  'CaribbeanCam': [
+    Badge.SUB_12_MONTHS_T1,
+  ],
+  'Bananaman3100': [
+    Badge.SUB_6_MONTHS_T1,
+  ],
+  'Betelgeun': [
+    Badge.SUB_12_MONTHS_T1,
+  ],
+  'HikkenNoAce': [
+    Badge.SUB_6_MONTHS_T3,
+  ],
+  'CannabisMuffin': [
+    Badge.SUB_18_MONTHS_T1,
+  ],
 };
 
 const SoundFilters_Array = {
@@ -3250,7 +3250,10 @@ function showContributors() {
       Array.from(document.getElementsByClassName('queue_entry'));
   for (const item of queuedItems) {
     // ex: "Added by: airforce2700"
-    const addedBy = item.attributes.getNamedItem('title').value;
+    const addedBy = item.getAttribute('title');
+    if (!addedBy) {
+      continue;
+    }
     const user = addedBy.replace('Added by: ', '');
     const current = userQueueCounts.get(user) || 0;
     userQueueCounts.set(user, current + 1);
