c688d66b441d8507a502d1bdf1fc3b928e7abe2a
papertek <littlerock894@gmail.com>
2022-01-23 02:26:08 -0600

    more stuff

--- main.js
+++ main.js
@@ -3735,7 +3735,7 @@ if (UI_CustomPingSound && CustomPingSound_URL != '') {
 
 // public skipping from ui
 if (UI_PublicSkip) {
-  $('#voteskip').click(function() {
+  $('#voteskip').on('click', function() {
     socket.emit('voteskip');
     socket.emit('chatMsg', {msg: '[red]Meh..[/] ResidentSleeper'});
     $('#voteskip').attr('disabled', true);
