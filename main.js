66bfb7e58aeef8bdb4b6b5fb05955efd96159008
papertek <littlerock894@gmail.com>
2022-02-05 16:01:01 -0600

    bump version

--- main.js
+++ main.js
@@ -190,7 +190,7 @@ const MiniLogo_URL = 'https://cdn.7tv.app/emote/614e8c0b20eaf897465a4c9d/1x';
 
 const ChannelName_Caption = 'CyDJ';
 
-const Version_Now = 'CyDJPre2.5.22.0';
+const Version_Now = 'CyDJPre2.5.22.1';
 
 const HeaderDropMenu_Title = 'Information';
 
