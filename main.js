dd4db430f1577ca440e1c51c7604bb253ef041b1
papertek <littlerock894@gmail.com>
2022-02-16 19:00:10 -0600

    cleanup

--- main.js
+++ main.js
@@ -1735,9 +1735,7 @@ function setUserCSS() {
     $('body').css('background-color', '#141414');
     $('.queue_entry').css('background-color', '#293d36bd');
     $('.dropdown-menu').css('background-color', '#383E40');
-    $('.btn-default').css('background-image', 'linear-gradient(#4c333e00,#4c333e00 60%,#4c333e00)');
-    $('.btn-default').css('border-color', '#4cb290');
-    $('.btn-default').css('border-color', '#3a3f4400');
+    $('.btn-default').css({'background-image': 'linear-gradient(#4c333e00,#4c333e00 60%,#4c333e00)', 'border-color': '#4cb290', 'border-color': '#3a3f4400'});
     $('.btn-default:hover').css('background-color', '#4cb290');
     $('#motd').css('background-color', '#fff0');
     $('#motdwrap').css('background-color', '#293d36bd');
@@ -1746,9 +1744,7 @@ function setUserCSS() {
     $('body').css('background-color', '');
     $('.queue_entry').css('background-color', '');
     $('.dropdown-menu').css('background-color', '');
-    $('.btn-default').css('background-image', '');
-    $('.btn-default').css('border-color', '');
-    $('.btn-default').css('border-color', '');
+    $('.btn-default').css({'background-image': '', 'border-color': '', 'border-color': ''});
     $('.btn-default:hover').css('background-color', '');
     $('#motd').css('background-color', '');
     $('#motdwrap').css('background-color', '');
