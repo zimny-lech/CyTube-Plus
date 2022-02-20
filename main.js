df6d936610fad9b11a4777889a7a1eb092869c35
papertek <littlerock894@gmail.com>
2022-02-20 16:02:03 -0600

    fix attempt

--- main.js
+++ main.js
@@ -1783,7 +1783,7 @@ function setUserCSS() {
   }`;
 
   $('head').append(`<style id="chanexternalcss-fix" type="text/css">${cssfix}</style>`);
-  $('head').append(`<style id="green-update" type="text/css">${greencss}</style>`);
+//  $('head').append(`<style id="green-update" type="text/css">${greencss}</style>`);
   $('#usertheme').attr('href', '/css/themes/slate.css');
   $('#usertheme-fix').remove();
   if (USERTHEME.includes('/css/themes/')) {
