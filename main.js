739af52fe789a4db52c99cbda3b54a827a24b39d
papertek <littlerock894@gmail.com>
2022-03-11 09:38:28 -0600

    restructure thing

--- main.js
+++ main.js
@@ -4556,8 +4556,39 @@ if (UI_Snow && Snow_URL !== '') {
   $.getScript(Snow_URL);
 }
 
-// Hey hey hey xqcPeepo here, I'm adding this emote format here just incase we want to inject emotes
-// ourselves or add them programmatically
+// I'm adding this emote format here just incase we want to inject emotes
+
+twemojiEnabled = false;
+$.getScript('https://twemoji.maxcdn.com/v/latest/twemoji.min.js', (successCallback) => {
+  // loading the twemojis so I don't have to manually add the emojis into r/cydj
+  tweEmojiList = $.getJSON('https://unpkg.com/emoji.json/emoji.json', (successCallback) => {
+    console.log(tweEmojiList);
+    tweEmojiList.responseJSON.forEach((index) => {
+      /* the first index returns something like, {codes: "1F600", char: "😀", name:
+                       'grinning face', category: 'Smileys & Emotion (face-smiling)', group:
+                       'Smileys & Emotion', subgroup: 'face-smiling'} */
+      const localemoteName = ':' + index.name.replace(' ', '-') + ':';
+      const localemoteImage = getTwEmojiImageFromEmoticode(index.char);
+      pushEmoteToWindow(localemoteName, localemoteImage);
+    });
+  });
+  const newLocal = '!!Loaded twemoji.js!!';
+  console.log(newLocal);
+  twemojiEnabled = true;
+  // to get fix previous chat messages that didn't have the emote parsed I will grab them now
+  const messagebufferlocal = document.getElementById('messagebuffer');
+  for (let child = messagebufferlocal.firstElementChild; child !== null;
+       child = child.nextElementSibling) {
+    child.querySelectorAll('span:not([class])')
+        .forEach((childElement) => {  // this is assuming we don't have any other classes for chat
+                                      // messages, which might change in the future but I'll
+                                      // update the code to reflect that as well
+          if (childElement !== null) {
+            twemoji.parse(childElement);
+          }
+        });
+  }
+});
 
 function pushEmoteToWindow(emoteName, emoteImage) {
   window.Callbacks.updateEmote(
@@ -4581,38 +4612,6 @@ function getTwEmojiImageFromEmoticode(textEmoticode) {
   return twEmojiImageURL;
 }
 
-twemojiEnabled = false;
-$.getScript('https://twemoji.maxcdn.com/v/latest/twemoji.min.js', (successCallback) => {
-  // xqcPeepo here, loading the twemojis so I don't have to manually add the emojis into r/cydj
-  tweEmojiList = $.getJSON('https://unpkg.com/emoji.json/emoji.json', (successCallback) => {
-    console.log(tweEmojiList);
-    tweEmojiList.responseJSON.forEach((index) => {
-      /* the first index returns something like, {codes: "1F600", char: "😀", name:
-                       'grinning face', category: 'Smileys & Emotion (face-smiling)', group:
-                       'Smileys & Emotion', subgroup: 'face-smiling'} */
-      const localemoteName = ':' + index.name.replace(' ', '-') + ':';
-      const localemoteImage = getTwEmojiImageFromEmoticode(index.char);
-      pushEmoteToWindow(localemoteName, localemoteImage);
-    });
-  });
-  const newLocal = '!!Loaded twemoji.js!!';
-  console.log(newLocal);
-  twemojiEnabled = true;
-  // to get fix previous chat messages that didn't have the emote parsed I will grab them now :
-  // xqcPeepo
-  const messagebufferlocal = document.getElementById('messagebuffer');
-  for (let child = messagebufferlocal.firstElementChild; child !== null;
-       child = child.nextElementSibling) {
-    child.querySelectorAll('span:not([class])')
-        .forEach((childElement) => {  // this is assuming we don't have any other classes for chat
-                                      // messages, which might change in the future but I'll
-                                      // update the code to reflect that as well
-          if (childElement !== null) {
-            twemoji.parse(childElement);  // xqcPeepo was here
-          }
-        });
-  }
-});
 /*
   https://github.com/twitter/twemoji#api
   in short : just use twemoji.parse(...), should place string or node in the first parameter and
