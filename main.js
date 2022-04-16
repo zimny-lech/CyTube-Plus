8d330d5c75549710ad4e990d3adc71b66598b735
papertek <littlerock894@gmail.com>
2022-04-16 03:19:22 -0500

    make stuff constant

--- main.js
+++ main.js
@@ -1132,10 +1132,10 @@ function progressBar() {
  * @param {string} div
  */
 function setPanelProperties(div) {
-  bgcolor = $('body').css('background-color');
-  color = $('body').css('color');
-  height = $('#userlist').height();
-  width = $('#userlist').width();
+  const bgcolor = $('body').css('background-color');
+  const color = $('body').css('color');
+  const height = $('#userlist').height();
+  const width = $('#userlist').width();
   $(div).css({
     'background-color': bgcolor,
     'color': color,
