96533420049fed53560dc1b7cf8560fdb8c5f47e
papertek <littlerock894@gmail.com>
2022-04-23 16:13:58 -0500

    do things with rewrite

--- CDJRewrite.js
+++
@@ -1,7 +0,0 @@
-// just something for now
-/*
-import {formatBadgeToHtml, USER_BADGES} from './lib/badges';
-import {CHANNEL_DATABASE} from './lib/database';
-import {LOGOS} from './lib/logos';
-import {initTwemoji} from './lib/twemoji';
-*/
