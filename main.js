38378a4c7089500a95e17bef979de5a1dc714a9f
papertek <littlerock894@gmail.com>
2022-06-11 12:22:36 -0500

    rewrite close button

--- main.js
+++ main.js
@@ -1094,11 +1094,12 @@ function changeMOTD() {
 function rulesModal() {
   createModal('Rules Panel');
   modalBody.append(RulesBtn_HTML);
-  const closebotan =
-      ($ < '<button class ="btn btn-default" />').text('Close').appendTo(modalFooter);
-  closebotan.on('click', () => {
-    modalOuter.modal('hide');
-  });
+  $('<button class ="btn btn-default" type="button" data-dismiss="modal"/>')
+      .text('Close')
+      .appendTo(modalFooter);
+  /*  closebotan.on('click', () => {
+      modalOuter.modal('hide');
+    });*/
 }
 
 /**
