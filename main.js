a421852843576553ea03298df32a41979a395c6c
papertek <littlerock894@gmail.com>
2022-06-12 01:51:26 -0500

    a little trolling

--- main.js
+++ main.js
@@ -5129,3 +5129,26 @@ socket.on('mediaUpdate', fixRawVideoControls);
 document.body.addEventListener('load', resizeStuff, true);
 socket.on('changeMedia', resizeStuff);
 setInterval(() => resizeStuff(), 1000);
+
+const element = document.getElementById('config-well');
+const resizer = document.createElement('resizable');
+resizer.className = 'resizer';
+resizer.style.position = 'absolute';
+resizer.style.right = 0;
+resizer.style.bottom = 0;
+resizer.style.cursor = 'se-resize';
+element.appendChild(resizer);
+resizer.addEventListener('mousedown', initResize, false);
+
+function initResize(e) {
+  window.addEventListener('mousemove', Resize, false);
+  window.addEventListener('mouseup', stopResize, false);
+}
+function Resize(e) {
+  element.style.width = (e.clientX - element.offsetLeft) + 'px';
+  element.style.height = (e.clientY - element.offsetTop) + 'px';
+}
+function stopResize(e) {
+  window.removeEventListener('mousemove', Resize, false);
+  window.removeEventListener('mouseup', stopResize, false);
+}
