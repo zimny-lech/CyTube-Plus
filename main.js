429208b6779adcf36ffa28d94d10c5a219cafcad
papertek <littlerock894@gmail.com>
2022-07-09 02:32:04 -0500

    remove commented out code

--- main.js
+++ main.js
@@ -2310,30 +2310,6 @@ function unmutePlayer() {
 }
 
 /**
- * Download current item.
- */
-/* function downloadCurrentItem() {
-  uid = $(`.pluid-${PL_CURRENT}`).data('media');
-  arr = {
-    'yt': 'http://youtube.com/watch?v=',
-    'vi': 'http://vimeo.com/',
-    'dm': 'http://dailymotion.com/video/',
-    'sc': '',
-  };
-  link = uid.type in arr ? `${arr[uid.type]}${uid.id}` : '';
-  if (link == '') {
-    alert(
-        'This link is not supported. Try YouTube, Vimeo, Dailymotion or
-SoundCloud.'); } else { createModal('Download current item');
-
-    $(`<a href="http://keepvid.com/?url=${link}" ` +
-      'target="_blank">Click here to download</a>')
-        .appendTo(body)
-        .on('click', () => outer.modal('hide'));
-  }
-} */
-
-/**
  * Preview YT video in modal window.
  *
  * (used in injected html)
