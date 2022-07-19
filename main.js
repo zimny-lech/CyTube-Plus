6d07829b90143ab877e2ffeecc68fe8de8806824
Peachy <106801886+ItMePeachy@users.noreply.github.com>
2022-07-19 17:27:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2274,7 +2274,7 @@ function coverPlayer() {
   }
   $('#videowrap').addClass('relative');
   const w = $('#ytapiplayer').css('width');
-  const h = $('#videowrap').css('height').replace('px', '') - 31;
+  const h = $('#videowrap').css('height').replace('px', '') - 30;
   coverpl = $('<div id="coverpl" />')
                 .css({
                   'width': w,
