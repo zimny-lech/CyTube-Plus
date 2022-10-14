2a2f83018239c5471190dff44d62b2991b81ab1d
papertek <littlerock894@gmail.com>
2022-10-14 17:07:06 -0500

    undo

--- rollup.config.js
+++ rollup.config.js
@@ -7,11 +7,5 @@ export default {
     format: 'iife',
     name: 'cydj',
   },
-  input: 'mainv2.js',
-  output: {
-    file: 'build/mainv2.js',
-    format: 'iife',
-    name: 'cydjv2',
-  },
   plugins: [nodeResolve()],
 };
