74bf7b09da15b3b68763b44975df689bfb02d93b
Peachy <106801886+ItMePeachy@users.noreply.github.com>
2022-11-03 07:37:56 -0600

    Update main.js

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ const MiniLogo_URL = 'https://cdn.7tv.app/emote/614e8c0b20eaf897465a4c9d/1x';
 
 const ChannelName_Caption = 'CyDJ';
 
-const Version_Now = 'CyDJPre10.15.22.0';
+const Version_Now = 'CyDJPre11.3.22.0';
 
 const HeaderDropMenu_Title = 'Information';
 
