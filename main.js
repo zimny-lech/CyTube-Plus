6ce7f43e065bb1737ae51b1812dc3f4b867d3e1c
papertek <littlerock894@gmail.com>
2022-11-26 15:05:34 -0600

    update motd logo

--- main.js
+++ main.js
@@ -262,7 +262,7 @@ const HeaderDropMenu_Array = [
 ];
 
 const MOTDAutoLogo_Array = [
-  'https://github.com/papertek/CyDJ/raw/beta/images/cydjhallo.jpg',
+  'https://github.com/papertek/CyDJ/raw/beta/images/cydjnormal.jpg',
 ];
 
 const MOTDTabs_Array = [
